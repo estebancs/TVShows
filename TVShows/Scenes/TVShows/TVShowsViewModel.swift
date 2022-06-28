36fc5a982306cce413685a01de550671a7a3a146
Esteban Chavarria <jossiestebancs@gmail.com>
2022-06-27 23:46:57 -0600

    Separate call to service in view model

--- TVShows/Scenes/TVShows/TVShowsViewModel.swift
+++ TVShows/Scenes/TVShows/TVShowsViewModel.swift
@@ -8,24 +8,23 @@
 import Combine
 import Resolver
 
+protocol TVShowsViewModeling {
+    var output: TVShowsViewOutput { get set}
+    func fetchTvShows()
+    func searchTvShow(text: String)
+}
+
+protocol TVShowsViewOutput: AnyObject {
+    func fetchedTvShows(tvShows: TVShows)
+    func foundSerchedTvShows(tvShows: TVShows)
+}
+
 class TVShowsViewModel {
     @Injected var service: TVShowsServicing
     
-    private let getTVShowsTrigger = PassthroughSubject<Void, Never>()
+    weak var _output: TVShowsViewOutput?
     
-    // MARK: - Input
-
-    private(set) lazy var input = {
-        Input(getTVShows: getTVShowsTrigger)
-    }()
-    
-    // MARK: - Output
-
-    private(set) lazy var output: Output = {
-        let episodesDataSource = PassthroughSubject<TVShows, Never>().eraseToAnyPublisher()
-        return Output(tvShowsDataSource: episodesDataSource)
-    }()
-    
+    var pageNumber = 0
     
     /// Fecths the list of tv shows
     /// - Parameters:
@@ -37,13 +36,62 @@ class TVShowsViewModel {
         }
     }
     
+    /// Search for tv shows based in a string
+    /// - Parameters:
+    ///     - text: searching text
+    ///     - completion: completion block after getting results
+    func searchTvShows(text: String, completion: @escaping (Result<[SearchResult], HttpError>) -> Void) {
+        Task(priority: .background) {
+            let result = await service.searchShow(text: text)
+            completion(result)
+        }
+    }
+    
 }
 
-extension TVShowsViewModel {
-    struct Input {
-        let getTVShows: PassthroughSubject<Void, Never>
+extension TVShowsViewModel: TVShowsViewModeling {
+    var output: TVShowsViewOutput {
+        get {
+            return _output!
+        }
+        set {
+            _output = newValue
+        }
     }
-    struct Output {
-        let tvShowsDataSource: AnyPublisher<TVShows, Never>
+    
+    
+    /// Calls the  service to browse the tv series and returns the result
+    func fetchTvShows() {
+        fetchTvShows(page: pageNumber) { [weak self] result in
+            switch result {
+            case .success(let newTvShows):
+                guard let `self` = self else { return }
+                self.pageNumber += 1
+                DispatchQueue.main.async {
+                    self._output?.fetchedTvShows(tvShows: newTvShows)
+                }
+            case .failure(let error):
+                print("error: \(error.errorMessage)")
+            }
+        }
+    }
+    
+    
+    /// Calls the search service and returns tv shows
+    /// - Parameters:
+    ///   - text: text to search
+    func searchTvShow(text: String) {
+        searchTvShows(text: text) { [weak self] result in
+            switch result {
+            case .success(let searchResult):
+                guard let `self` = self else { return }
+                let filteredTVShows = searchResult.map{$0.show}
+                DispatchQueue.main.async {
+                    self._output?.foundSerchedTvShows(tvShows: filteredTVShows)
+                }
+            case .failure(let error):
+                print("error: \(error.errorMessage)")
+            }
+        }
     }
 }
